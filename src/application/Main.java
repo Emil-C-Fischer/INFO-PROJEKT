e01155175ca9a3e6b194a9b01c511ab0710cd620
Emil-C-Fischer <emil-fischer@gmx.de>
2019-06-12 14:29:22 +0200

    hinzugefügt: Bilder, Abfrage textfield

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import java.awt.Insets;
 
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -17,14 +18,34 @@ import javafx.event.EventHandler;
 
 
 public class Main extends Application {
-    boolean istgruen = false;
+	
+	GRAPH graph1 = new GRAPH();
+	
 
+	
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
+    	
+ 
+    	
+    	graph1.knotenEinfuegen(new RAUM("raum1", "r1", 1));
+    	graph1.knotenEinfuegen(new RAUM("raum2", "r2", 2));
+    	graph1.knotenEinfuegen(new RAUM("raum3", "r3", 3));
+    	graph1.knotenEinfuegen(new RAUM("raum4", "r4", 4));
+    	graph1.knotenEinfuegen(new RAUM("raum5", "r5", 5));
+    	
+    	graph1.standortSetzen("r1");
+    	
+    	System.out.println(graph1.gibStandort());
+    	
     	Pane root = new Pane();
 
+        Scene scene = new Scene(root, 1880, 950);
+
+        primaryStage.setTitle("ADVENTURE");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    	
         //Control panel
         TextField tf1 = new TextField ();
         tf1.setLayoutX(384);
@@ -41,6 +62,7 @@ public class Main extends Application {
         Image minimap1 = new Image("/minimap/minimap1.jpg");
         Image tempel1 = new Image("/main/tempel1.jpg");
         Image testtext = new Image("/text/testtext.jpg");
+        Image spielbeginnt = new Image("/text/spiel-beginnt.jpg");
 
         //picture fields
         ImageView main = new ImageView();
@@ -60,7 +82,7 @@ public class Main extends Application {
 
         //Buttons
         Button btn1 = new Button();
-        btn1.setText("btn1");
+        btn1.setText("start!");
         btn1.setLayoutX(384);
         btn1.setLayoutY(850);
         btn1.setPrefHeight(70);
@@ -68,14 +90,14 @@ public class Main extends Application {
 
 
         Button btn2 = new Button();
-        btn2.setText("btn2");
+        btn2.setText("---");
         btn2.setLayoutX(604);
         btn2.setLayoutY(850);
         btn2.setPrefHeight(70);
         btn2.setPrefWidth(200);
 
         Button btn3 = new Button();
-        btn3.setText("btn3");
+        btn3.setText("---");
         btn3.setLayoutX(824);
         btn3.setLayoutY(850);
         btn3.setPrefHeight(70);
@@ -86,18 +108,25 @@ public class Main extends Application {
         root.getChildren().add(btn2);
         root.getChildren().add(btn3);
 
-
+        
+        
         btn1.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-            	//
+            	textfield.setImage(spielbeginnt);
             }
         });
+        
+        tfb.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override public void handle (ActionEvent e) {
+                if(tf1.getText() == "test") {
+                	System.out.println("richtig");
+                } else {
+                	System.out.println("falsch");
+                }
+        	}
+        });
+        
 
-        Scene scene = new Scene(root, 1880, 950);
-
-        primaryStage.setTitle("ADVENTURE");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
 
